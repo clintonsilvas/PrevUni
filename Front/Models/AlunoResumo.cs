273fd8520a78d391c983c80ca5c18e2fa8f69e72
RafaelOdeCLopes <rafaeloclopes2509@gmail.com>
2025-05-22 17:51:16 -0300

    Exibição parcial do perfil de aluno

---
+++ Front/Models/AlunoResumo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Front.Models
+{
+    public class AlunoResumo
+    {
+        public string user_id { get; set; }
+        public string nome { get; set; }
+        public string ultimo_acesso { get; set; }
+        public int total_acessos { get; set; }
+        public int dias_ativos { get; set; }
+        public Dictionary<string, int> interacoes_por_componente { get; set; }
+        public List<string> cursos_acessados { get; set; }
+    }
+
+    public class AcaoQtd
+    {
+        public string Acao { get; set; }
+        public int Quantidade { get; set; }
+    }
+
+    public class UsuarioComAcoes
+    {
+        public string UserId { get; set; }
+        public string Nome { get; set; }
+        public List<AcaoQtd> Acoes { get; set; }
+    }
+}
