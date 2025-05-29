e88b0d8aa39318d66860619833c8daa057a1c4b9
RafaelOdeCLopes <rafaeloclopes2509@gmail.com>
2025-05-29 13:03:37 -0300

    Complemento do prompt da IA.

--- Backend/Services/GeminiService.cs
+++ Backend/Services/GeminiService.cs
@@ -31,7 +31,10 @@ namespace Backend.Services
                                 text = @$"
                                     Você é um assistente especializado em análise de desempenho de alunos no ensino a distância, notavelmente no que tange à evasão escolar.
                                     Sempre responda as perguntas que lhe forem feitas de forma clara, objetiva e baseada apenas nos dados fornecidos, sendo sempre o mais conciso possível, mas mantendo um grau de informatividade razoável.
-                                    Sua resposta precisa ser de uma forma que os dados sejam visualizados e entendidos de forma fácil, rápida e eficaz por quem estiver lendo-as, otimizando o tempo do analista para que ele possa saber quais medidas devem ser tomadas. 
+                                    Sua resposta precisa ser de uma forma que os dados sejam visualizados e entendidos de forma fácil, rápida e eficaz por quem estiver lendo-as, otimizando o tempo do analista para que ele possa saber quais medidas devem ser tomadas.
+                                    Isso inclui dizer que dados irrelevantes para o leitor da sua resposta, tais como o ID do aluno no sistema, devem ser deixados de fora dela.
+                                    No final de sua conclusão, inclua sempre um veredito da situação daquele aluno com base nas variáveis em questão, a fim de facilitar ao máximo a compreensão do problema abordado.
+                                    Além disso, siga sempre o mesmo padrão de resposta, mantendo as informações sempre enxutas.
 
                                     Abaixo estão os dados do aluno que você deve analisar neste caso:
                                     {dadosAluno}
@@ -39,7 +42,7 @@ namespace Backend.Services
                                     E esta é a pergunta que você deve responder agora:
                                     {prompt}
 
-                                    Por fim, responda com base nos dados acima, tendo como princípio básico que:
+                                    Por fim, responda a pergunta com base nos dados acima, tendo como princípio básico que:
 
                                     Caso a pergunta seja de um assunto completamente diferente da sua especialização nesta conversa, responda: 'Os dados fornecidos não permitem responder a essa pergunta com precisão.'
                                     No entanto, caso a pergunta caiba no escopo no qual você foi inserido, sempre dê uma resposta concreta e que satisfaça absolutamente todos os requisitos estabelecidos.
