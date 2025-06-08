1878261cf5cf60d2582ed877e91b7e4398787bfc
Samuel Nogueira <samucans2004@gmail.com>
2025-06-08 13:01:00 -0300

    Adicionei ação de quizz

--- Front/Models/Acoes.cs
+++ Front/Models/Acoes.cs
@@ -14,6 +14,7 @@
     {
         new Acoes { nome_acao = "Atividade Concluída", action = "graded", target = "user", component = "core" },
         new Acoes { nome_acao = "Postagem no Fórum", action = "uploaded", target = "assessable", component = "mod_forum" },
+        new Acoes { nome_acao = "Realizou um Quizz", action = "submitted", target = "attempt", component = "mod_quiz" },
 
         //new Acoes { nome_acao = "Visualizar Disciplina", action = "viewed", target = "course", component = "core" },
         //new Acoes { nome_acao = "Visualizar Tela com todos os Emblemas", action = "viewed", target = "badge_listing", component = "core" },
@@ -24,12 +25,13 @@
         //new Acoes { nome_acao = "Visualizar Fórum", action = "viewed", target = "course_module", component = "mod_forum" },
         //new Acoes { nome_acao = "Visualizou Respostas do Fórum", action = "viewed", target = "discussion", component = "mod_forum" },
 
-        new Acoes { nome_acao = "Clicou em Recurso (PDF, vídeo...)", action = "viewed", target = "course_module", component = "mod_resource" },
-        new Acoes { nome_acao = "Clicou em Link de Unidade", action = "viewed", target = "course_module", component = "mod_url" },
+        //new Acoes { nome_acao = "Iniciou Quizz", action = "started", target = "attempt", component = "mod_quiz" },  
+        //new Acoes { nome_acao = "Clicou em Recurso (PDF, vídeo...)", action = "viewed", target = "course_module", component = "mod_resource" },
+        //new Acoes { nome_acao = "Clicou em Link de Unidade", action = "viewed", target = "course_module", component = "mod_url" },
         //new Acoes { nome_acao = "Criou Resposta no Fórum", action = "created", target = "discussion", component = "mod_forum" },
         //new Acoes { nome_acao = "Criou Assinatura de Discussão", action = "created", target = "discussion_subscription", component = "mod_forum" },
         //new Acoes { nome_acao = "Enviou Resposta no Fórum", action = "uploaded", target = "assessable", component = "mod_forum" },
-        new Acoes { nome_acao = "Postou em Discussão do Fórum", action = "created", target = "post", component = "mod_forum" }
+        //new Acoes { nome_acao = "Postou em Discussão do Fórum", action = "created", target = "post", component = "mod_forum" }
     };
         }
 
