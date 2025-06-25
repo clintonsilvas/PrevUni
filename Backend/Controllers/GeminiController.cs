e5755a82ad1b500740608476c7fd442cab7cd1b5
RafaelOdeCLopes <rafaeloclopes2509@gmail.com>
2025-06-25 19:40:30 -0300

    Correção de erros

--- Backend/Controllers/GeminiController.cs
+++ Backend/Controllers/GeminiController.cs
@@ -1,41 +1,27 @@
 ﻿using Backend.Services;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
 
 namespace Backend.Controllers
 {
     public class GeminiController : Controller
     {
         private readonly GeminiService _geminiService;
-
         public GeminiController(GeminiService geminiService)
         {
             _geminiService = geminiService;
         }
+
         public class PerguntaIaRequest
         {
             public string Prompt { get; set; }
-            public string DadosAluno { get; set; }
-            public int pos { get; set; }
+            public string Dados { get; set; }
         }
+
+
         [HttpPost("pergunte-ia")]
         public async Task<IActionResult> PergunteIA([FromBody] PerguntaIaRequest request)
-        {
-            var resposta = await _geminiService.EnviarPromptAsync(request.Prompt, request.DadosAluno, request.pos);
-            if (string.IsNullOrEmpty(resposta))
-                return NotFound("IA não respondeu.");
-
-            return Ok(new { respostaIA = resposta });
-        }
-
-
-        public class PerguntaIaCursoRequest
-        {
-            public string Prompt { get; set; }
-            public string DadosCurso { get; set; }
-        }
-
-        [HttpPost("pergunte-ia-nomeCurso")]
-        public async Task<IActionResult> PergunteIACurso([FromBody] PerguntaIaCursoRequest request)
         {
             if (request == null)
                 return BadRequest(new { erro = "Request inválido: corpo nulo." });
@@ -43,10 +29,10 @@ namespace Backend.Controllers
             if (string.IsNullOrEmpty(request.Prompt))
                 return BadRequest(new { erro = "Prompt é obrigatório." });
 
-            if (string.IsNullOrEmpty(request.DadosCurso))
+            if (string.IsNullOrEmpty(request.Dados))
                 return BadRequest(new { erro = "Dados do nomeCurso são obrigatórios." });
 
-            var resposta = await _geminiService.EnviarPromptAsync(request.Prompt, request.DadosCurso, 0);
+            var resposta = await _geminiService.EnviarPromptAsync(request.Prompt, request.Dados);
 
             if (string.IsNullOrEmpty(resposta))
             {
@@ -55,6 +41,7 @@ namespace Backend.Controllers
 
             return Ok(new { respostaIA = resposta });
         }
+
 
 
     }
