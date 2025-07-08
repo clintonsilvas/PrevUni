caf6a611dad72223d0441631cee989795f04b453
Clinton Nogueira Silva <clintonnogueirasilva@gmail.com>
2025-07-07 21:57:01 -0300

    Testes deploy

--- Front/Program.cs
+++ Front/Program.cs
@@ -2,16 +2,16 @@ using Front.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
-// Adicione esta linha para registrar IHttpClientFactory
+
 builder.Services.AddHttpClient();
 builder.Services.AddSingleton<FavoritoService>();
 
 builder.Services.AddSession();
 builder.Services
        .AddRazorPages()
-       .AddSessionStateTempDataProvider();   // mantém TempData em sessão se quiser
+       .AddSessionStateTempDataProvider(); 
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -25,11 +25,10 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 app.UseSession();
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error");    
     app.UseHsts();
 }
 
@@ -37,9 +36,7 @@ if (!app.Environment.IsDevelopment())
 app.UseCors();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapGet("/", context =>
@@ -49,40 +46,5 @@ app.MapGet("/", context =>
 });
 
 app.MapRazorPages();
-
-
-app.MapPost("/api/favoritos/adicionar_curso", (HttpRequest request) =>
-{
-    var curso = request.Query["nomeCurso"].ToString();
-    FavoritoService.AdicionarCurso(curso);
-    return Results.Ok();
-});
-
-app.MapPost("/api/favoritos/remover_curso", (HttpRequest request) =>
-{
-    var curso = request.Query["nomeCurso"].ToString();
-    FavoritoService.RemoverCurso(curso);
-    return Results.Ok();
-});
-
-
-app.MapPost("/api/favoritos/adicionar_aluno", (HttpRequest request) =>
-{
-    var nome = request.Query["name"].ToString();
-    var id = request.Query["id"].ToString();
-    FavoritoService.AdicionarAluno(nome, id);
-    return Results.Ok();
-});
-
-app.MapPost("/api/favoritos/remover_aluno", (HttpRequest request) =>
-{
-    var nome = request.Query["name"].ToString();
-    var id = request.Query["id"].ToString();
-    FavoritoService.RemoverAluno(nome, id);
-    return Results.Ok();
-});
-
-
-
 
 app.Run();
