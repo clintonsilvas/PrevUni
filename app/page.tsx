dc606db796561f224ef63f59d065da478d5493d0
Clinton Nogueira Silva <126696407+clintonsilvas@users.noreply.github.com>
2026-07-16 12:33:25 +0000

    Removendo Tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div>
-      <main >      
-        <h1>FRONT-END SIMPATIA</h1> 
-        <img title="logo" src="/logo.svg"></img>      
-        <a href="/api" target="_blank">Teste o backend aqui</a>     
-      </main>
-    </div>
-  );
+  redirect("/login");
 }
--- app/teste/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Teste() {
-  return (
-    <div className="bg-red-500 p-20 m-20 rounded-xl text-white text-4xl">
-      TESTE
-    </div>
-  );
-}
